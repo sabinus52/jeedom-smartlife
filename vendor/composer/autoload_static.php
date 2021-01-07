e8d37bf48ba39c0380cc0d0dd913720ab73a8e8a
Sabinus52 <sabinus52@gmail.com>
2021-01-07 21:31:37 +0100

    Fix v2 cannot auth exced once in 60s #52

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,12 +10,15 @@ class ComposerStaticInit2f8b093fee39f02e3b1d0e00489f0051
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
+            'Symfony\\Polyfill\\Ctype\\' => 23,
+            'Symfony\\Component\\Filesystem\\' => 29,
             'Sabinus\\TuyaCloudApi\\' => 21,
         ),
         'P' => 
@@ -31,6 +34,14 @@ class ComposerStaticInit2f8b093fee39f02e3b1d0e00489f0051
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Ctype\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'Symfony\\Component\\Filesystem\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/filesystem',
+        ),
         'Sabinus\\TuyaCloudApi\\' => 
         array (
             0 => __DIR__ . '/..' . '/sabinus52/tuyacloudapi/src',
